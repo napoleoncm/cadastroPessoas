d3fd37f2815feae50923098f63120e4bb0b26e8e
Caique Marques <63432870+napoleoncm@users.noreply.github.com>
2023-03-27 10:50:43 -0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cadastroPessoas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cadastroPessoas.Models
+{
+    public class Pessoa
+    {
+
+        [Key]
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+    }
+}
